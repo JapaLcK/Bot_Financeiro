59ff9aac5458f49e1c2d79f9b7895b6a37c40378
Lucas <lucaskuramoti06@gmail.com>
2026-02-07 18:09:29 -0500

    mudancas no grafico de pizza

--- bot.py
+++ bot.py
@@ -1360,8 +1360,8 @@ async def on_message(message: discord.Message):
             pie.title = "Distribuição das despesas"
             pie.add_data(data, titles_from_data=True)
             pie.set_categories(cats)
-            pie.height = 7
-            pie.width = 8
+            pie.height = 12
+            pie.width = 15
             pie.style = 10
 
             ws_dash.add_chart(pie, "G1")
